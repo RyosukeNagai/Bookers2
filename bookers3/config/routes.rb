b3f87339206166daca063a7847e852d6b88fdc74
長井崚介 <ryooo@nagainoMacBook-Pro.local>
2020-06-04 23:01:20 +0900

    [いいね機能追加]

--- bookers3/config/routes.rb
+++ bookers3/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users,only: [:show,:index,:edit,:update]
   resources :books do
   resource :book_comments, only: [:create]
+  resource :favorites, only: [:create, :destroy]
   end
   resources :book_comments, only: [:destroy]
   
